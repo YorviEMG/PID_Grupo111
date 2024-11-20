da00bfa71e18ed72531d193dce9c9e6f19dbfef9
Eduardo <Jorgeeduardo03062004@gmail.com>
2024-11-20 11:54:54 -0500

    Commit 1-Q

--- app/src/main/java/com/example/pid/Entidad/Usuario.kt
+++ app/src/main/java/com/example/pid/Entidad/Usuario.kt
@@ -14,6 +14,7 @@ class Usuario {
     var fechaNacimiento: String = ""
 
     //idUsuario:Int,
+    //Se genera constructor para incializar los datos
     constructor(nombres:String, apellidos:String, dni:String,
                 login:String, password:String, email:String, fechaNacimiento: String
     ){
