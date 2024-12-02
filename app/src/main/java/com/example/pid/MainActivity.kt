995fe53f4b9db263f4df54ea03ccf20b581e7478
ivanmoreno-alumnocibertec <i202120930@cibertec.edu.pe>
2024-12-01 22:38:09 -0500

    Login, falta retocar

--- app/src/main/java/com/example/pid/MainActivity.kt
+++ app/src/main/java/com/example/pid/MainActivity.kt
@@ -7,9 +7,26 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import android.content.Intent
+import android.util.Log
 import android.widget.Button
+import android.widget.Toast
+import com.example.appproject.utils.ApiUtils
+import com.example.pid.Entidad.LoginRequest
+import com.example.pid.Entidad.ResponseMessage
+import com.example.pid.Entidad.Usuario
+import com.example.pid.service.ApiServiceUsuario
+import com.google.android.material.textfield.TextInputEditText
+import retrofit2.Call
+import retrofit2.Response
+import javax.security.auth.callback.Callback
 
 private lateinit var btnRegistro: Button
+private lateinit var btnLogin: Button
+private lateinit var txtUsuarioLogin: TextInputEditText
+private lateinit var txtUsuarioContrasena: TextInputEditText
+
+private lateinit var api: ApiServiceUsuario
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,11 +40,26 @@ class MainActivity : AppCompatActivity() {
         }
         btnRegistro = findViewById(R.id.btnRegistro)
         btnRegistro.setOnClickListener{ goRegistro()}
+
+        txtUsuarioLogin = findViewById(R.id.txtUsuarioLogin)
+        txtUsuarioContrasena = findViewById(R.id.txtUsuarioContrasena)
+        btnLogin = findViewById(R.id.btnLogin)
+        btnLogin.setOnClickListener{login()}
+
+        api = ApiUtils.getAPIServiceUsuarioLogin()
     }
     fun goRegistro(){
         var intent = Intent(this, RegistroActivity::class.java)
         startActivity(intent)
     }
+
+
+    fun goToMenu(){
+        var intent = Intent(this, MenuActivity::class.java)
+        startActivity(intent)
+    }
+
+
     fun showAlert(men:String){
         val builder= AlertDialog.Builder(this)
         builder.setTitle("Información")
@@ -35,5 +67,41 @@ class MainActivity : AppCompatActivity() {
         builder.setPositiveButton("Aceptar",null)
         val dialog: AlertDialog =builder.create()
         dialog.show()
+    }
+
+    fun login(){
+        val login = txtUsuarioLogin.text.toString()
+        val password = txtUsuarioContrasena.text.toString()
+
+        if (login.isEmpty()) {
+            showAlert("Ingrese su correo.")
+            return
+        }
+        else if (password.isEmpty()) {
+            showAlert("Ingrese su contraseña.")
+            return
+        }
+
+
+        // Falta arreglar el login
+        val bean = LoginRequest(password, login)
+        api.postLogin(bean).enqueue(object : retrofit2.Callback<ResponseMessage> {
+            override fun onResponse(call: Call<ResponseMessage>, response: Response<ResponseMessage>) {
+                if (response.isSuccessful) {
+                    val obj1 = response.body()!!
+                    Log.d("MainActivity", obj1.mensaje)
+                    showAlert(obj1.mensaje)
+                    goToMenu();
+                } else {
+                    showAlert("Error en el ingreso. Intente nuevamente.")
+                    //goToMenu();
+                }
+            }
+
+            override fun onFailure(call: Call<ResponseMessage>, t: Throwable) {
+                Log.e("MainActivity", "Error de red: ${t.localizedMessage}")
+                showAlert("Error de red. Intente nuevamente.")
+            }
+        })
     }
 }
--- app/src/main/java/com/example/pid/util/ApiUtils.kt
+++ app/src/main/java/com/example/pid/util/ApiUtils.kt
@@ -4,10 +4,16 @@ import com.example.pid.service.ApiServiceUsuario
 
 class ApiUtils {
     companion object {
-        const val BASE_URL = "http://todo-list-api-net.eba-tmtufgav.us-east-2.elasticbeanstalk.com/api/"
+        const val BASE_URL = "http://todo-list-api-net.eba-tmtufgav.us-east-2.elasticbeanstalk.com/"
+
+        const val BASE_URL_LOGIN = "http://todo-list-api-net.eba-tmtufgav.us-east-2.elasticbeanstalk.com/url/"
 
         fun getAPIServiceUsuario(): ApiServiceUsuario {
             return RetrofitClient.getClient(BASE_URL).create(ApiServiceUsuario::class.java)
+        }
+
+        fun getAPIServiceUsuarioLogin(): ApiServiceUsuario {
+            return RetrofitClient.getClient(BASE_URL_LOGIN).create(ApiServiceUsuario::class.java)
         }
     }
 }
